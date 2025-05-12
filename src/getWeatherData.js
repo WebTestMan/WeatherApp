d26dd12c61bf78df94c32a1479e11e7a18108f7f
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-12 17:05:54 +0100

    Basic get weather api call created

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -2,6 +2,16 @@ const getWeatherData = (function () {
   // hit api
   // extract data
   // return different bits of data
+  const weatherRod = "WKCG34RNMQGB6SNVKC9BQEB9M";
+
+  const getWeather = (location = "London") => {
+    fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=uk&key=${weatherRod}`
+    ).then((response) => {
+      console.log(response.json());
+    });
+  };
+  return { getWeather };
 })();
 
 export default getWeatherData;
