e9501fc5461762efaaef827194edda7e4cfe4823
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-12 17:06:58 +0100

    basic gif api call created

--- src/getGiphy.js
+++ src/getGiphy.js
@@ -1,7 +1,19 @@
 const getGiphy = (function () {
-  // hit api
-  // extract data
-  // return img url
+  const getGif = (gifToSearchFor) => {
+    //   const img = document.querySelector('img');
+    fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=mOlnNSnubcajFdrUrjEEV6GWdSrqIJ0f&s=${gifToSearchFor}`,
+      { mode: "cors" }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response.data.images.original.url);
+        return response.data.images.original.url;
+      });
+  };
+  return { getGif };
 })();
 
 export default getGiphy;
