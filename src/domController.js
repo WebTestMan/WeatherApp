764142de7c5693df3cd3c0f8626f62ca8a6ebd8f
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-12 13:36:39 +0100

    started domcontroller design

---
+++ src/domController.js
@@ -0,0 +1,9 @@
+const domController = (function () {
+  // update display
+  // draw weather div
+  // update temp
+  // update conditions
+  // update img, take in new url as arg
+})();
+
+export default domController;
