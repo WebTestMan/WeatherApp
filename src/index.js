de94bd4c9e9aad0128e82b18c341418ec74b1028
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-15 14:56:27 +0100

    added input form function and updated search

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import "./styles.css";
 import getWeatherData from "./getWeatherData";
-import getGiphy from "./getGiphy";
+// import getGiphy from "./getGiphy";
+import domController from "./domController";
 
 // search box, calls getWeather Data
 
-const weatherDisplayDiv = document.getElementById("weather-input-div");
-const weatherGif = document.createElement("img");
-const imgURL = getGiphy.getGif("dog");
-weatherGif.setAttribute("src", imgURL);
-weatherDisplayDiv.appendChild(weatherGif);
+// const weatherDisplayDiv = document.getElementById("weather-input-div");
+// const weatherGif = document.createElement("img");
+// const imgURL = await getGiphy.getGif("dog");
+// weatherGif.setAttribute("src", imgURL);
+// weatherDisplayDiv.appendChild(weatherGif);
 
-const weatherSearchLocation = document.getElementById("weather-Input");
-weatherSearchLocation.addEventListener("submit", () => {
+domController.drawWeatherInput();
+
+const weatherSearchLocationForm = document.getElementById("weather-Input");
+
+weatherSearchLocationForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
   const weatherLocation = document.getElementById("weather-Location").value;
-  console.log(weatherLocation);
-  const weatherData = getWeatherData.getWeather(weatherLocation);
-  console.log(weatherData);
+  // console.log(weatherLocation);
+  const weatherData = await getWeatherData.getWeather(weatherLocation);
+  // console.log(weatherData);
+  domController.updateWeatherDisplay(weatherData);
 });
-
- const weatherData = getWeatherData.getWeather('London');
- console.log(weatherData);
