95ac71cd308a5a462b5a3b7cecc6e0c88b74580a
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-15 14:56:01 +0100

    Moved input form into domController

--- src/domController.js
+++ src/domController.js
@@ -1,9 +1,62 @@
+import getWeatherData from "./getWeatherData";
+
 const domController = (function () {
   // update display
   // draw weather div
   // update temp
   // update conditions
   // update img, take in new url as arg
+
+  const weatherDisplayDiv = document.getElementById("weather-display");
+
+  const drawWeatherInput = () => {
+    const weatherInputDiv = document.createElement("div");
+    weatherInputDiv.setAttribute("id", "weather-input-div");
+    const weatherInputForm = document.createElement("form");
+    weatherInputForm.setAttribute("id", "weather-Input");
+    const weatherInputBox = document.createElement("input");
+    const weatherInputBoxAttributesArray = {
+      id: "weather-Location",
+      name: "weather-Location",
+      placeholder: "Please enter a location...",
+    };
+    for (const property in weatherInputBoxAttributesArray) {
+      weatherInputBox.setAttribute(
+        property,
+        weatherInputBoxAttributesArray[property]
+      );
+    }
+    const weatherButton = document.createElement("button");
+    weatherButton.innerText = "Get Weatherly";
+
+    weatherInputForm.appendChild(weatherInputBox);
+    weatherInputForm.appendChild(weatherButton);
+    weatherInputDiv.appendChild(weatherInputForm);
+    weatherDisplayDiv.appendChild(weatherInputDiv);
+  };
+
+  const updateWeatherDisplay = (weatherData) => {
+    console.log(weatherData);
+    weatherDisplayDiv.innerHTML = "";
+
+    const weatherDetails = [];
+
+    const currentConditions = document.createElement("p");
+    currentConditions.innerText =
+      getWeatherData.getCurrentConditions(weatherData);
+    weatherDetails.push(currentConditions);
+
+    const currentTemp = document.createElement("p");
+    currentTemp.innerText = getWeatherData.getCurrentTemp(weatherData);
+    weatherDetails.push(currentTemp);
+
+    weatherDetails.forEach((detail) => {
+      console.log(detail);
+      weatherDisplayDiv.appendChild(detail);
+    });
+  };
+
+  return { drawWeatherInput, updateWeatherDisplay };
 })();
 
 export default domController;
